867251be04cfc5c362c54997b09c37bfcd26b037
Rafa Ricardo <alumno27000@gmail.com>
2025-05-05 18:00:08 +0000

    5_mayo hasear tabla promos

--- models/staging/sql_server_dbo/base/base_sql_server_dbo_promos.sql
+++ models/staging/sql_server_dbo/base/base_sql_server_dbo_promos.sql
@@ -1,22 +1,30 @@
-{{
-  config(
-    materialized='view'
-  )
-}}
+{{ config(materialized="view") }}
 
-WITH src_promos AS (
-    SELECT * 
-    FROM {{ source('sql_server_dbo', 'promos') }}
+with
+    src_promos as (
+        select *
+        from {{ source("sql_server_dbo", "promos") }}
+
+        union all
+
+        select
+            'desconocido' as promo_id,
+            0 as discount,
+            'inactive' as status,
+            null as _fivetran_deleted,
+            null as _fivetran_synced
+
     ),
 
-renamed_casted AS (
-    SELECT
-          generate (promo_id)
-        , product_id
-        , quantity
-        , month
-        , _fivetran_synced AS date_load
-    FROM src_promos
+    renamed_casted as (
+        select
+            {{ dbt_utils.generate_surrogate_key(["promo_id"]) }} as promo_id,
+            promo_id as descripcion,
+            discount,
+            status,
+            _fivetran_synced as date_load
+        from src_promos
     )
 
-SELECT * FROM renamed_casted
+select *
+from renamed_casted
