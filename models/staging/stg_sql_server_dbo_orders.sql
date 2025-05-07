e281ea9ca6cbc94b983da1b8201d653bf060d55e
Rafa Ricardo <alumno27000@gmail.com>
2025-05-07 11:03:17 +0000

    cambios 7 de mayo , stg y base

---
+++ models/staging/stg_sql_server_dbo_orders.sql
@@ -0,0 +1,20 @@
+{{ config(materialized="view") }}
+
+select
+    {{ dbt_utils.generate_surrogate_key(['order_id']) }} as order_id_sk,
+    order_id,
+    user_id,
+    promo_id,
+    address_id,
+    shipping_service,
+    shipping_cost,
+    order_cost,
+    order_total,
+    tracking_id,
+    status,
+    convert_timezone('UTC', created_at) as created_at_utc,
+    convert_timezone('UTC', estimated_delivery_at) as estimated_delivery_utc,
+    convert_timezone('UTC', delivered_at) as delivered_at_utc,
+    convert_timezone('UTC', _fivetran_synced) as synced_at_utc,
+    _fivetran_deleted
+from {{ source('sql_server_dbo', 'orders') }}
