241cdcb79c6fea9fefa8cd037ba80a2f1686f62e
Rafa Ricardo <alumno27000@gmail.com>
2025-05-15 17:52:29 +0000

    dia 15

--- models/staging/stg_sql_server_dbo__products.sql
+++ models/staging/stg_sql_server_dbo__products.sql
@@ -1,28 +1,25 @@
 {{ 
   config(
-    materialized = 'view',
-    database     = env_var('DBT_ENVIRONMENTS') ~ '_SILVER_DB',
-    schema       = 'SQL_SERVER_DBO',
-    tags         = ['staging']
+    materialized = 'view'
   ) 
 }}
 
 with base as (
-  select * from {{ ref('base_sql_server_dbo__order_items') }}
+  select * 
+  from {{ ref('base_sql_server_dbo__products') }}
 ),
 
 cleaned as (
   select
-    -- surrogate key sobre order_id + product_id
-    {{ dbt_utils.generate_surrogate_key(['order_id_nk', 'product_id_nk']) }} as order_item_sk,
-    order_id_nk,
-    product_id_nk,
-    coalesce(quantity_raw, 0) as quantity,
-    -- synced timestamp a UTC
-    convert_timezone('UTC', synced_at_raw)::timestamp_tz as synced_at_utc,
-    _fivetran_deleted
+    product_id_nk       as product_id_nk,
+    name_raw            as name_raw,
+    price_raw           as price_raw,
+    inventory_raw        as inventory_raw,
+    _fivetran_deleted,
+    synced_at_raw as synced_at_raw
   from base
   where coalesce(_fivetran_deleted, false) = false
 )
 
-select * from cleaned
+select * 
+from cleaned
